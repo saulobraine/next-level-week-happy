6cb66cee8087cbf0cebcf2abbd70dca1e3b35efc
Saulo Braine <saulo@braine.dev>
2020-10-13 19:02:57 -0300

    Aula 2 - Olhando as oportunidades

---
+++ backend/src/errors/handler.ts
@@ -0,0 +1,29 @@
+import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
+import fs from 'fs';
+
+interface ValidationErrors {
+  [key: string]: string[];
+}
+
+const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
+
+  if (error instanceof ValidationError) {
+    let errors: ValidationErrors = {};
+
+    error.inner.forEach(err => {
+      errors[err.path] = err.errors;
+    });
+
+    // Remove upload files on validation error
+    const requestImages = request.files as Express.Multer.File[];
+    requestImages.map(image => fs.unlinkSync(image.path));
+
+    return response.status(400).json({ message: 'Validation fails', errors });
+
+  }
+
+  return response.status(500).json({ message: 'Internal server error' });
+}
+
+export default errorHandler;
